f3b9a2d70147a582be1a195a4dee6c51bfd16e5b
MaRah <martin.lars.rahm@gmail.com>
2021-04-07 17:37:19 +0200

    search error message added

--- src/App.js
+++ src/App.js
@@ -6,68 +6,85 @@ import SearchBar from "./components/SearchBar";
 import { FaRandom } from "react-icons/fa";
 
 function App() {
-	// States
-	const [randomDrink, setRandomDrink] = useState(null);
-	const [selectedDrink, setSelectedDrink] = useState(null);
-	const [searchResults, setSearchResults] = useState(null);
-	const [searchTerm, setSearchTerm] = useState(null);
+  // States
+  const [randomDrink, setRandomDrink] = useState(null);
+  const [selectedDrink, setSelectedDrink] = useState(null);
+  const [searchResults, setSearchResults] = useState(null);
+  const [searchTerm, setSearchTerm] = useState(null);
+  const [searchMessage, setSearchMessage] = useState(false);
 
-	// Fetches
-	async function randomData() {
-		const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/random.php`);
-		const data = await response.json();
-		setRandomDrink(data.drinks[0]);
-		setSearchResults(null);
-		setSelectedDrink(null);
-	}
+  // Fetches
+  async function randomData() {
+    setSearchMessage(false);
+    const response = await fetch(
+      `https://www.thecocktaildb.com/api/json/v1/1/random.php`
+    );
+    const data = await response.json();
+    setRandomDrink(data.drinks[0]);
+    setSearchResults(null);
+    setSelectedDrink(null);
+  }
 
-	async function fetchByName() {
-		const response = await fetch(
-			`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchTerm}`
-		);
-		const data = await response.json();
-		setSearchResults(data.drinks);
-	}
+  async function fetchByName() {
+    setSearchMessage(false);
+    const response = await fetch(
+      `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchTerm}`
+    );
+    const data = await response.json();
+    setSearchResults(data.drinks);
 
-	useEffect(() => {
-		randomData();
-	}, []);
+    if (!data.drinks) {
+      setSearchMessage(true);
+    }
+  }
 
-	return (
-		<div className="subpixel-antialiased">
-			<header className="bg-gray-700 flex justify-center items-center h-16">
-				<button
-					className="rounded w-12 h-12 bg-green-300 text-red-500 cursor-pointer mx-5 flex	justify-center items-center"
-					onClick={randomData}
-				>
-					<FaRandom />
-				</button>
+  useEffect(() => {
+    randomData();
+  }, []);
 
-				<SearchBar
-					fetchByName={fetchByName}
-					searchTerm={searchTerm}
-					setSearchTerm={setSearchTerm}
-					setSelectedDrink={setSelectedDrink}
-				/>
-			</header>
+  return (
+    <div className="subpixel-antialiased">
+      <header className="bg-gray-700 flex justify-center items-center h-16">
+        <button
+          className="rounded w-12 h-12 bg-green-300 text-red-500 cursor-pointer mx-5 flex	justify-center items-center"
+          onClick={randomData}
+        >
+          <FaRandom />
+        </button>
 
-			<main className="min-h-screen w-full flex flex-col items-center content-start">
-				{!searchResults && randomDrink
-					? !selectedDrink && <DrinkDetails props={randomDrink} />
-					: null}
-				{searchResults && (
-					<SearchList
-						searchResults={searchResults}
-						setSearchResults={setSearchResults}
-						setSelectedDrink={setSelectedDrink}
-					/>
-				)}
-				{selectedDrink ? (
-					<DrinkDetails props={selectedDrink.drink} setSearchResults={setSearchResults} />
-				) : null}
-			</main>
-		</div>
-	);
+        <SearchBar
+          fetchByName={fetchByName}
+          searchTerm={searchTerm}
+          setSearchTerm={setSearchTerm}
+          setSelectedDrink={setSelectedDrink}
+        />
+      </header>
+
+      <main className="min-h-screen w-full flex flex-col items-center content-start">
+        {searchMessage && (
+          <span className="bg-indigo-50 p-2 my-2 text-red-500 text-sm md:text-lg xl:text-xl rounded">
+            No results found, but here's a random drink!
+          </span>
+        )}
+        {!searchResults && randomDrink
+          ? !selectedDrink && <DrinkDetails props={randomDrink} />
+          : null}
+        {searchResults && (
+          <SearchList
+            searchResults={searchResults}
+            setSearchResults={setSearchResults}
+            setSelectedDrink={setSelectedDrink}
+          />
+        )}
+        {selectedDrink ? (
+          <DrinkDetails
+            props={selectedDrink.drink}
+            setSearchResults={setSearchResults}
+          />
+        ) : null}
+      </main>
+    </div>
+  );
 }
 
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,27 +1,27 @@
 function SearchBar(props) {
-	return (
-		<div>
-			<form
-				onSubmit={(e) => {
-					e.preventDefault();
-					props.setSelectedDrink(null);
-					props.fetchByName();
-				}}
-			>
-				<input
-					type="text"
-					onChange={(e) => props.setSearchTerm(e.target.value)}
-					className="p-1 text-sm rounded-l outline-none"
-					placeholder="Search for a drink!"
-				/>
-				<input
-					type="submit"
-					value="🔎"
-					className="py-1 px-3 bg-green-300 text-red-500 cursor-pointer text-sm rounded-r outline-none	"
-				/>
-			</form>
-		</div>
-	);
+  return (
+    <div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          props.setSelectedDrink(null);
+          props.fetchByName();
+        }}
+      >
+        <input
+          type="text"
+          onChange={(e) => props.setSearchTerm(e.target.value)}
+          className="p-1 text-sm rounded-l outline-none"
+          placeholder="Search for a drink!"
+        />
+        <input
+          type="submit"
+          value="🔎"
+          className="py-1 px-3 bg-green-300 text-red-500 cursor-pointer text-sm rounded-r outline-none	"
+        />
+      </form>
+    </div>
+  );
 }
 
 export default SearchBar;
